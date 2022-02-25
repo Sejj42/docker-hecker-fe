9c464c44135c9efac323ae1db495ecbd2dec6947
Sejj42 <omer162@gmail.com>
2022-02-25 16:46:50 +0200

    .

--- pages/api/projects/[slug].js
+++ pages/api/projects/[slug].js
@@ -1,9 +1,9 @@
 const { projects } = require("./data.json");
 
 export default function handler(req, res) {
-  const evt = projects.filter((ev) => ev.slug === req.query.slug);
+  const project = projects.filter((proj) => proj.slug === req.query.slug);
   if (req.method === "GET") {
-    res.status(200).json(evt);
+    res.status(200).json(project);
   } else {
     res.setHeader("Allow", ["GET"]);
     res.status(405).json({ message: `Method ${req.method} is not allowed.` });
